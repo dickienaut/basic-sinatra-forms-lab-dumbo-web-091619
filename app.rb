5866ef2c294c3ed32f6699a67cb90d7cd2e02032
lyle dickie <lyle.e.dickie@gmail.com>
2019-10-10 14:26:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
-
+  
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '/team' do
+    binding.pry
+    @name = name
+    erb :team
+  end
 
 end
